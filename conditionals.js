8f7f9d53506e99de0f8de321f550241358c4509d
Mahsan-code <mahsanpan@gmail.com>
2020-12-29 21:19:33 +0000

    add a raining var.

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,15 @@
 const temp = 12;
-
+raining = true;
 if(temp<0){
     console.log('Make sure you pick out a scarf');
-}else if (temp<15){
+}else if (temp<15 && raining){
     console.log('short sleevs wont cut it');
+}else if(temp<-40 || temp>40){
+    console.log('maybe going outside is not a great idea');
 }else{
     console.log('short sleevs are fine');
 }
 
 console.log('now you are ready to go out');
+
+
