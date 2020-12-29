7b71ff89e2e8e657cc735ebe53645b753c91db04
Mahsan-code <mahsanpan@gmail.com>
2020-12-29 23:24:15 +0000

    for and while loop through array-- printing reverse array

---
+++ packing-list.js
@@ -0,0 +1,16 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+for(let i=0; i<packingList.length;i++){
+
+    console.log(packingList[i]); 
+}
+
+let i=0;
+while(i<packingList.length){
+    console.log(packingList[i]); 
+    i++;
+}
+for(let i=packingList.length-1; i>=0;i--){
+
+    console.log(packingList[i]); 
+}
